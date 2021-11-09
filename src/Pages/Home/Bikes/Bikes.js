d3ac112c1126e0c484d8ad5a7e1c90f645c2f7de
Hamed166 <hamed.mgt.cu@gmail.com>
2021-11-10 01:54:42 +0600

    navigation added to home page

---
+++ src/Pages/Home/Bikes/Bikes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bikes = () => {
+    return (
+        <div>
+            <h2>This is Bikes Page</h2>
+        </div>
+    );
+};
+
+export default Bikes;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
 
 const Home = () => {
     return (
         <div>
+            <Navigation></Navigation>
+            
             <h2>This is Home</h2>
         </div>
     );
